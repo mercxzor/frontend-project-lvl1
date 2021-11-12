b2e5bd1744bfd297875dc6d0b6060bf4c5c30fcc
Artem Gaynulin <stmercs@gmail.com>
2021-11-12 15:08:18 +0500

    update brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node.
-console.log('Welcome to the Brain Games!');
+console.log("Welcome to the Brain Games!");
+import { greet } from "/home/artem/frontend-project-lvl1/src/cli.js";
+greet();
