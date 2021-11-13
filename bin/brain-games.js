fa9e3d91cb07e0e85888a0cb4e1b9ccbadb37a16
Artem Gaynulin <stmercs@gmail.com>
2021-11-13 14:03:48 +0500

    edit path in brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node.
 
-// eslint-disable-next-line import/no-absolute-path
-import greet from '/home/artem/frontend-project-lvl1/src/cli.js';
+import greet from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 greet();
